8d68c01e94891cd13723db29cf8ab938e625e338
majesk <thiagomgoulart@gmail.com>
2019-07-06 19:42:11 -0300

    Upgrade to AspNetCore 2.2

--- Project.Web/Startup.cs
+++ Project.Web/Startup.cs
@@ -30,7 +30,7 @@ namespace Project.Web
         {
             services.AddSession();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(opts =>
